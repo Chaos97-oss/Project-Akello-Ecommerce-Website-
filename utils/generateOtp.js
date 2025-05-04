37aacae7abe8546299f999c35efe645003d75c86
Paul <udegbuepaul43@gmail.com>
2025-05-04 17:02:48 +0100

    NodeCache added for Email OTP for orders(Untested)

--- utils/generateOtp.js
+++ utils/generateOtp.js
@@ -1,3 +1,6 @@
+import NodeCache from 'node-cache'
+import sendEmail from "../utils/sendEmail.js"
+const otpCache = new NodeCache({ stdTTL: 300 }); // 5 minutes
 const generateOtp = () => {
   const otp = Math.floor(100000 + Math.random() * 900000); // 6 digit
   const expiry = Date.now() + 10 * 60 * 1000; // 10 minutes
