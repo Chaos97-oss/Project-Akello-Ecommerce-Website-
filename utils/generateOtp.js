958fca0b6c98d6e7e4163e3719a28ed58c454711
Paul <udegbuepaul43@gmail.com>
2025-05-06 15:02:53 +0100

    OTP generate route for Pre order

--- utils/generateOtp.js
+++ utils/generateOtp.js
@@ -1,7 +1,7 @@
 import NodeCache from 'node-cache'
 import sendEmail from "../utils/sendEmail.js"
 const otpCache = new NodeCache({ stdTTL: 300 }); // 5 minutes
-const generateOtp = () => {
+export const generateOtp = () => {                            //remove the export !!!!
   const otp = Math.floor(100000 + Math.random() * 900000); // 6 digit
   const expiry = Date.now() + 10 * 60 * 1000; // 10 minutes
   return { otp, expiry };
