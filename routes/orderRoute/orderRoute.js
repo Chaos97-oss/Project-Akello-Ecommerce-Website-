579f4b5a22b8bfd9c9433815d65f2624ae6056fc
Paul <udegbuepaul43@gmail.com>
2025-05-10 14:34:24 +0100

    Authentication middleware added and OrderOTP tested

--- routes/orderRoute/orderRoute.js
+++ routes/orderRoute/orderRoute.js
@@ -9,6 +9,6 @@ router.get("/my-orders", protect, getUserOrders);
 router.post("/", protect, createOrder);
 router.put("/update-status/:orderId", protect, isAdmin, updateOrderStatus);
 router.delete("/:orderId", protect, isAdmin, deleteOrder);
-router.post("/send-otp", sendOrderOtp)
-router.post("/verify-otp", verifyOrderOtp)
+router.post("/send-otp", protect, sendOrderOtp)
+router.post("/verify-otp", protect, verifyOrderOtp)
 export default router;
